4f549f10e03a0c49d34eb2ebdc83ad1372770d33
heheh <heheh@something.com>
2024-08-16 21:00:58 +0700

    hheheh

--- image_converter.py
+++ image_converter.py
@@ -120,22 +120,10 @@ class ImageConverter:
         """
         max_length = self.pipeline.tokenizer.model_max_length
 
-        # simple way to determine length of tokens
-        count_prompt = len(prompt.split(" "))
-        count_negative_prompt = len(negative_prompt.split(" "))
-
-        # create the tensor based on which prompt is longer
-        if count_prompt >= count_negative_prompt:
-            input_ids = self.pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
-            shape_max_length = input_ids.shape[-1]
-            negative_ids = self.pipeline.tokenizer(negative_prompt, truncation=False, padding="max_length",
-                                              max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
-
-        else:
-            negative_ids = self.pipeline.tokenizer(negative_prompt, return_tensors="pt", truncation=False).input_ids.to(device)
-            shape_max_length = negative_ids.shape[-1]
-            input_ids = self.pipeline.tokenizer(prompt, return_tensors="pt", truncation=False, padding="max_length",
-                                           max_length=shape_max_length).input_ids.to(device)
+        input_ids = self.pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+        shape_max_length = input_ids.shape[-1]
+        negative_ids = self.pipeline.tokenizer("", truncation=False, padding="max_length",
+                                          max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
 
         concat_embeds = []
         neg_embeds = []
